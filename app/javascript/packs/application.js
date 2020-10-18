a32bd652efd1add55991bbaaf2c449659e3daed1
Rajat Subhra Ghosh <Rajat.Ghosh@oracle.com>
2020-10-17 23:58:08 -0700

    Updated .gitignore file and changed the stylesheet file organization

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'bootstrap';
+import '../stylesheets/application.scss';
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -15,6 +17,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-import 'bootstrap';
-import './src/application.scss';
