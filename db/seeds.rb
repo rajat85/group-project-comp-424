38fc680262c37f783681dd48c11c86026f27b13f
Rajat Subhra Ghosh <Rajat.Ghosh@oracle.com>
2020-11-05 20:29:31 -0800

    Fixed registration isssues

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+SecurityQuestion.create! locale: :en, name: "What is your mother's maiden name?"
+SecurityQuestion.create! locale: :en, name: "Where were you born?"
+SecurityQuestion.create! locale: :en, name: "What is the name of your first pet?"
+SecurityQuestion.create! locale: :en, name: "What's your favorite movie?"
+SecurityQuestion.create! locale: :en, name: "What's your favorite book?"
+SecurityQuestion.create! locale: :en, name: "What is your favorite animal?"
+SecurityQuestion.create! locale: :en, name: "What is your favorite travel destination?"
