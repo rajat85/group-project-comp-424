848b493a0a86dff28b766f5f5ebdc6e276f2b6bc
Niraj Pathak <58009976+niraj-pathak@users.noreply.github.com>
2020-11-07 18:56:51 -0800

    Added HOME_DIR in installation script
    To make the script more flexible.

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+HOME_DIR=/home/ubuntu
 echo "Installing the required technology stack"
 
 
@@ -39,7 +39,7 @@ sudo /usr/sbin/passenger-memory-stats
 # insall rvm using curl for ruby
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /home/ubuntu/.rvm/scripts/rvm
+source $HOME_DIR/.rvm/scripts/rvm
 
 #Start mysql
 sudo service mysql start
