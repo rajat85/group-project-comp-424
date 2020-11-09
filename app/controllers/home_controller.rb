e477242f0083d38bc15648f005af0674cc29aedd
Rajat Subhra Ghosh <Rajat.Ghosh@oracle.com>
2020-11-09 01:55:31 -0800

    Download file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,23 @@
+require "prawn"
+
 class HomeController < ApplicationController
   def index
   end
+
+  def download_pdf
+    send_data generate_pdf(current_user),
+              filename: "#{current_user.first_name}-#{current_user.last_name}.pdf",
+              type: "application/pdf"
+  end
+
+  private
+
+  def generate_pdf(user)
+    Prawn::Document.new do
+      text "#{user.first_name} #{user.last_name}", align: :center
+      text "Email: #{user.email}"
+      text "Date of birth: #{user.date_of_birth.try(:strftime,"%m/%d/%Y")}"
+    end.render
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
       sessions: 'users/sessions'
   }
+  get "home/download_pdf"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
