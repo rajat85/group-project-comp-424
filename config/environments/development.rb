05dc8832121660ba49d170d0547b1716b60bb91f
Rajat Subhra Ghosh <Rajat.Ghosh@oracle.com>
2020-11-14 00:31:39 -0800

    Added host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.hosts << "ec2-18-218-71-170.us-east-2.compute.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
