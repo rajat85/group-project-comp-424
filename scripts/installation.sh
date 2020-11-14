92aa9cdfe4434816dfc131c35ef32eb36a0e3cd9
Niraj Pathak <58009976+niraj-pathak@users.noreply.github.com>
2020-11-14 00:03:11 -0800

    Update installation script

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -8,36 +8,37 @@ echo "Installing the required technology stack"
 sudo apt update
 sudo apt update -y
 
-# Install Apache2, MySQL, SNORT
-sudo apt install apache2 
+# Install Nginx, MySQL, openssl, openssh server
+sudo apt install nginx
 sudo apt -y install mysql-server
 sudo apt install -y openssh-server
 sudo apt install -y gcc libpcre3-dev zlib1g-dev libluajit-5.1-dev libpcap-dev openssl libssl-dev libnghttp2-dev libdumbnet-dev bison flex libdnet
-sudo apt install yarn
 
-# install mysql client
+#Install mysql client
 sudo apt-get install libmysqlclient-dev
 
-# Install PGP key and add HTTPS support for APT
+#Install NodeJS
+sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#Install yarn
+sudo curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
+#Install Passenger module for Nginx 
 sudo apt-get install -y dirmngr gnupg
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
 sudo apt-get install -y apt-transport-https ca-certificates
-
-# Add APT repository for fusion passenger
 sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
 sudo apt-get update
+sudo apt-get install -y libnginx-mod-http-passenger
 
-# Install FusionPassenger server + Apache module
-sudo apt-get install -y libapache2-mod-passenger
-sudo a2enmod passenger
-
-echo "Validate the Fusion Passenger installation"
-
+#Validate passenger install
 echo | sudo /usr/bin/passenger-config validate-install 
-
 sudo /usr/sbin/passenger-memory-stats
 
-# insall rvm using curl for ruby
+#Insall rvm using curl for ruby
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 source $HOME_DIR/.rvm/scripts/rvm
@@ -45,19 +46,15 @@ source $HOME_DIR/.rvm/scripts/rvm
 #Start mysql
 sudo service mysql start
 
-#Start Apache Web Server
-sudo systemctl start apache2
-sudo apache2ctl restart
+#Start NginX
+sudo systemctl start nginx
 
-# Allow to run Apache on boot up
-sudo systemctl enable apache2
+# Allow to run mysql and Nginx on boot up
 sudo systemctl enable mysql
+sudo systemctl enable nginx
 
 # Adjust Firewall
-sudo ufw allow in "Apache Full"
-
-# Allow Read/Write for Owner
-sudo chmod -R 0755 /var/www/html/
+sudo ufw allow 'Nginx Full'
 
 # update ruby version
 rvm install ruby-2.7.1
@@ -67,8 +64,7 @@ rvm use 2.7.1 --default
 
 
 echo -e "\n"
-
-apache2 -v
+nginx -v
 mysqld --version
 rvm list
 
